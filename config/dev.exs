1c4cf25a38449164afcf9cd92103cbdc544bf15b
likithmanoj <likithmanojm@gmail.com>
2025-04-13 19:20:40 +0530

    commit from Ubuntu

--- config/dev.exs
+++ config/dev.exs
@@ -3,13 +3,12 @@ import Config
 # Configure your database
 config :hello_app, HelloApp.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "",
   hostname: "localhost",
   database: "hello_app_dev",
-  stacktrace: true,
+  port: 5432,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
-
 # For development, we disable any cache and enable
 # debugging and code reloading.
 #
